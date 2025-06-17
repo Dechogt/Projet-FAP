8425f3a96778ce0dc44604808c86c56126bf68b9
Dechogt <dechardinlepro@gmail.com>
2025-06-17 09:47:48 +0200

    Erreurs de fusion

--- Travel_Paradise_Web/TravelParadise/src/Controller/HomeController.php
+++ Travel_Paradise_Web/TravelParadise/src/Controller/HomeController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        // Si l'utilisateur est connecté, le rediriger vers le dashboard
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        // Sinon, rediriger vers la page de connexion
+        return $this->redirectToRoute('app_login');
     }
 }
